b74ccc1a6e051b83020c64789468ea0177e1e758
Broadtech <fahad@broadtech-innovations.com>
2020-09-30 12:47:32 +0530

    Studio Customization module

--- studio_customization/models/stock.py
+++ studio_customization/models/stock.py
@@ -8,7 +8,11 @@ from odoo import api, fields, models, SUPERUSER_ID, _
 class Picking(models.Model):
     _inherit = "stock.picking"
     
+    currency_id = fields.Many2one('res.currency', compute='_get_company_currency', readonly=True)
+    
     x_studio_untaxed_amount = fields.Monetary(related='purchase_id.amount_untaxed', string="Untaxed Amount", store=True, readonly=False)
 
 
-
+    def _get_company_currency(self):
+        for picking in self:
+            picking.currency_id = picking.company_id.currency_id
