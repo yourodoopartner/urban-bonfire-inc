58a5112b4e397f309aa6ea8017f7be06e39bd66f
Broadtech <fahad@broadtech-innovations.com>
2021-01-05 10:49:32 +0530

    Updation Script module

---
+++ account_invoice_updation_script/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Account Invoice Updation Script V13 Migration',
+    'version': '13.0.1.0',
+    'category': 'custom',
+    'description': u"""
+Account Invoice Updation Script After V13 database Migration
+""",
+    'author': 'Broadtech',
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'invoice_updation.xml',
+    ],
+    'application': False,
+    'license': 'OPL-1',
+}
