25902c50443fd291b44b979e077ce504180fd81d
Broadtech <fahad@broadtech-innovations.com>
2021-01-11 16:06:18 +0530

    Creating PO per SO

---
+++ bt_po_per_so/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Single PO per SO',
+    'version': '13.0.1.0',
+    'category': 'Sales, Purchase',
+    'description': u"""
+This module is for creating Single PO per SO.
+""",
+    'author': 'Urban Bonfire Inc.',
+    'depends': [
+        'sale_stock', 'purchase_stock',
+    ],
+    'data': [
+    ],
+    'application': False,
+    'license': 'OPL-1',
+}
